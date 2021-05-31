227b601b6cbf95a69aca542eac95649557ebe5ee
Shahrukh <themrsrk@gmail.com>
2021-05-31 14:56:37 -0700

    Login Form and its output file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
-
-void main (){
-  runApp (MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
-      home : Scaffold(
-        body: Text(" Name: Shahrkukh Anjum  " "  Email: themrsrk@gmail.com"),
-    
-      )
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Center(child: Text("Login Page")),
+            ),
+            body: SingleChildScrollView(
+              child: Center(
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 50),
+                      Container(width: 200, child: TextField()),
+                      SizedBox(height: 30),
+                      Container(width: 200, child: TextField()),
+                      SizedBox(height: 30),
+                      ElevatedButton(onPressed: (){}, child: Text("Login"))
+                    ],
+                  ),
+                ),
+              ),
+            )));
   }
 }
